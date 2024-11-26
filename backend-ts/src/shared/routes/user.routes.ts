5c72834a3d0e29ad74728d63d3b47fb0ba44feaf
rida <rida.akhsas@agil-it.fr>
2024-11-26 11:26:52 +0100

    add: profile get enpoint

--- backend-ts/src/shared/routes/user.routes.ts
+++ backend-ts/src/shared/routes/user.routes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import authMiddleware from '../middleware/auth.middleware.js';
-import { saveProfile } from '../../modules/user/profile.controller.js';
+import { saveProfile, getProfile } from '../../modules/user/profile.controller.js';
 import { validateData } from '../utils/validationMiddleware.js';
 import { profileSchema } from '../../modules/user/profile.validation.js';
 const router = express.Router();
@@ -10,5 +10,5 @@ router.get('/protected', authMiddleware, (req, res) => {
     res.send({message: 'This is a public route'});
 });
 router.post('/user/profile', authMiddleware, validateData(profileSchema), saveProfile);
-
+router.get('/user/profile', authMiddleware, getProfile);
 export default router;
